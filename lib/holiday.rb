2cf2c233a76cf8edbac4a113f7660faca4c2646b
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-30 15:32:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 bbq_hash = []
-holilday_hash.each do |season, holidays|
+holilday_supplies.each do |season, holidays|
 holidays.each do |holiday, supplies|
   if supplies.include?("BBQ")
     bbq_hash << holiday
