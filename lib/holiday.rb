642b544d0bda5667c238fbf1e2f4a632f41c8f6c
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-30 15:18:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holidays|
   puts "#{season.capitalize}:"
   holidays.each do |holiday, supplies|
-  puts  "#{holiday.to_s.split.map(&:capitalize).join('_')}: #{supplies.join(', ')}" 
+  puts  "  #{holiday.to_s.split.map(&:capitalize).join('_')}: #{supplies.join(', ')}" 
 
 end
 
